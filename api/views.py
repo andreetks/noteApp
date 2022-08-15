e7b28916cde0df85c401137f86f4634ab02cfe1a
gonzalo <gonzalo.mugaburu1099@gmail.com>
2022-08-15 17:25:19 -0500

    first commit

---
+++ api/views.py
@@ -0,0 +1,90 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+
+# Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in' +
+            'post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes)
+
+
+@api_view(['GET', 'POST'])
+def getNotes(request):
+
+    if request.method == 'GET':
+        notes = Note.objects.all().order_by('-updated_at')
+        serializer = NoteSerializer(notes, many=True)
+
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        data = request.data
+        try:
+            lastId = NoteSerializer(Note.objects.all().last()).data['id']
+        except KeyError:
+            lastId = 0
+        note = Note.objects.create(
+            id=lastId + 1,
+            title=f'Note {lastId + 1}',
+            content=data['content']
+        )
+        serializer = NoteSerializer(note, many=False)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getSingleNote(request, pk):
+
+    if request.method == 'GET':
+        note = Note.objects.get(id=pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        data = request.data
+        note = Note.objects.get(id=pk)
+        serializer = NoteSerializer(instance=note, data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        note = Note.objects.get(id=pk)
+        note.delete()
+        return Response('Note was deleted!')
