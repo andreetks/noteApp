aa74b9410a8ffcb836c3833eae6d2645ba869516
gonzalo <gonzalo.mugaburu1099@gmail.com>
2022-08-17 12:09:22 -0500

    Fix React

--- noteapp/settings.py
+++ noteapp/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = 'frontend/build/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'fronend/build/static'
 ]
 
 # Default primary key field type
